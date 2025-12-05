b0909a9f947e325bc27927147f2d4120ef6e9dd1
BuddyWinte <BuddyWinte@gmail.com>
2025-12-04 21:24:15 -0600

    Update file URL in version.js

--- version.js
+++ version.js
@@ -1,6 +1,6 @@
 {
   "version": "0.0.10",
   "files": [
-    "https://raw.githubusercontent.com/BuddyWinte/WintesGames/index.js",
+    "https://raw.githubusercontent.com/BuddyWinte/WintesGames/main/index.html",
   ]
 }
